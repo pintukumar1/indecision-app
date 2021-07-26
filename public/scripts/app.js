ba8510ca720e5f6e592f05f51eb787683cfb91ec
Pintu Kumar <kumarpintu1713122@gmail.com>
2021-07-26 15:38:17 +0530

    first commit

---
+++ public/scripts/app.js
@@ -0,0 +1,63 @@
+'use strict';
+
+console.log('App.js is running!');
+
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
+};
+
+var onFormSubmit = function onFormSubmit(event) {
+  event.preventDefault();
+
+  console.log('Form Submitted');
+};
+
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options' : 'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  ),
+  React.createElement(
+    'form',
+    { onSubmit: onFormSubmit() },
+    React.createElement('input', { type: 'text', name: 'option' }),
+    React.createElement(
+      'button',
+      null,
+      'Add Option'
+    )
+  )
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
